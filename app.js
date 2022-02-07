933ce59e7d7fd1a9d5200dfa94fd5ee5cdb9ed25
viraj <vpadale86@gmail.com>
2022-02-07 12:49:38 +0530

    hashing implemented by removing encryption

--- app.js
+++ app.js
@@ -4,17 +4,14 @@ const express = require("express");
 const ejs = require("ejs")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose");
-const encrypt=require("mongoose-encryption")
+const md5=require("md5");
 
 mongoose.connect("mongodb://localhost:27017/userDB")
-console.log(process.env.API_KEY);
 
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 })
-userSchema.plugin(encrypt, { secret: process.env.SECRET ,encryptedFields: ['password']});
-
 
 const User = new mongoose.model("User", userSchema);
 
@@ -40,7 +37,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
   const newUser = new User ({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   })
   newUser.save(function(err) {
     if (!err) {
@@ -56,9 +53,9 @@ app.post("/login", function(req, res) {
     if (err) {
       console.log(err);
     } else {
-      if (foundUser.password === req.body.password) {
+      if (foundUser.password === md5(req.body.password)) {
         res.render("secrets")
-        console.log(foundUser.password);
+        
       }
     }
   })
